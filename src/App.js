8181684a1bc7a34b6adecd424506333d4cdeefb3
leslie-alldridge <leslie.alldridge@gmail.com>
2019-01-19 20:31:43 +1300

    consuming api and rendering basic data

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    data: []
+  };
+  componentDidMount() {
+    try {
+      axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+      axios.defaults.xsrfCookieName = "csrftoken";
+      axios.get("/bucketlists/").then(data => {
+        this.setState({
+          data: data.data
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.data.map(item => {
+          return (
+            <p key={item.date_created}>
+              {item.name} created on: {item.date_created}
+            </p>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;

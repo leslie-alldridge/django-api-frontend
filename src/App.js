bfc191f94ba2297d8aa9ec2945ac9f3a1c0ba35e
leslie-alldridge <leslie.alldridge@gmail.com>
2019-01-20 10:17:43 +1300

    settings

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ class App extends Component {
   };
   componentDidMount() {
     try {
-      axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-      axios.defaults.xsrfCookieName = "csrftoken";
-      axios.get("/bucketlists/").then(data => {
+   
+      axios.get("https://django-backend-sqlite.herokuapp.com/bucketlists/").then(data => {
         console.log(data);
         
         this.setState({
